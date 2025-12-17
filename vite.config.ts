82efa2d16d2cc775be8d97f03a120633ccac719d
Beratemek <emekberat19@gmail.com>
2025-12-17 14:44:23 +0300

    Vite base ayari yapildi

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/the-vault/", // <--- TAM OLARAK BURAYA EKLEDİK (GitHub Pages için)
   server: {
-    host: '0.0.0.0', // Tüm bağlantılara izin ver
-    port: 5173, // Çalıştığınız portu belirtin
-    
-    // ------------------------------------------------------------------
-    // Hatanın istediği kesin çözüm: ALLOWED HOSTS
-    // Ngrok URL'sini buraya tırnaklar içinde ekleyin!
-    allowedHosts: [
-      'tatum-nonequable-verdie.ngrok-free.dev' // BURAYA GÜNCEL NGROK ADRESİNİZİ YAZIN
-    ],
-    // ------------------------------------------------------------------
-    
+    host: "0.0.0.0",
+    port: 5173,
+
+    allowedHosts: ["tatum-nonequable-verdie.ngrok-free.dev"],
+
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
+      "/api": {
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
